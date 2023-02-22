06feb5340472585383ae49feaebc8df907b2a8c4
Jered Ye <87924680+JeredYe@users.noreply.github.com>
2023-02-22 15:38:28 +0800

    或许你并不知道，你的某个朋友是你的亲戚。  他可能是你的曾祖父的外公的女婿的外甥女的表姐的孙子。  如果能得到完整的家谱，判断两个人是否是亲戚应该是可行的，但如果两个人的最近公共祖先与他们相隔好几代，使得家谱十分庞大，那么检验亲戚关系实非人力所能及。  在这种情况下，最好的帮手就是计算机。  为了将问题简化，你将得到一些亲戚关系的信息，如Marry和Tom是亲戚，Tom和Ben是亲戚，等等。  从这些信息中，你可以推出Marry和Ben是亲戚。  请写一个程序，对于我们的关于亲戚关系的提问，以最快的速度给出答案。  输入格式 输入由两部分组成。  第一部分以 N,M  开始。N  为问题涉及的人的个数。这些人的编号为 1,2,3,…,N 。下面有 M  行，每行有两个数 ai,bi ，表示已知 ai  和 bi  是亲戚。  第二部分以 Q  开始。以下 Q  行有 Q  个询问，每行为 ci,di ，表示询问 ci  和 di  是否为亲戚。  输出格式 对于每个询问 ci,di ，输出一行：若 ci  和 di  为亲戚，则输出“Yes”，否则输出“No”。  数据范围 1≤N≤20000 , 1≤M≤106 , 1≤Q≤106 输入样例： 10 7 2 4 5 7 1 3 8 9 1 2 5 6 2 3 3 3 4 7 10 8 9 输出样例： Yes No Yes

---
+++ 1249. 亲戚.cpp
@@ -0,0 +1,65 @@
+﻿//#include <iostream>
+//#include <cstdio>
+//using namespace std;
+//bool mp[20010][20010];
+//int m, n, q;
+//bool check(int aim, int b) {
+//	if (mp[b][aim])return 1;
+//	for (int i = 1; i <= n; ++i) {
+//		bool status = false;
+//		if (mp[b][i])status = check(aim, i);
+//		if (status)return 1;
+//	}
+//	return 0;
+//}
+//int main() {
+//	
+//	cin >> n >> m;
+//	ios::sync_with_stdio(0);
+//	while (m--) {
+//		int a, b;
+//		scanf("%d%d", &a, &b);
+//		mp[a][b] = mp[b][a] = 1;
+//	}
+//	cin >> q;
+//	while (q--) {
+//		int c, d;
+//		scanf("%d%d", &c, &d);
+//		cout << (check(c,d) ? "Yes" : "No") << endl;
+//	}
+//	return 0;
+//}
+///以上代码为未学习并查集时所写，
+
+
+#include <iostream>
+using namespace std;
+int m, n, q;
+int st[20010];
+int find(int x) {
+	if (x == st[x])return x;//如果所在的集合是自己本身（要么只有自己一个元素，要么自己是集合的“代表”），则返回自己
+	st[x] = find(st[x]);//否则的话则更新x位置所在的集合，例如x=5,st[x]=3时，查询位置3属于哪个集合...返回给st[x](以递归的方式)
+	return st[x];//返回位置x所在的集合编号.
+}
+void merge(int a, int b) {
+	int setA = find(a), setB = find(b);
+	if (setA != setB)st[setA] = st[setB];
+	//由于是单向操作的，所以不用担心是否会在查找过程中死循环，比如st[3]=5,st[5]=3
+}
+int main(){
+	cin >> n >> m;
+	for (int i = 1; i <= n; ++i)st[i] = i;
+	while (m--) {
+		int a, b;
+		scanf("%d%d", &a, &b);
+		merge(a, b);
+	}
+	cin >> q;
+	while (q--) {
+		int c, d;
+		scanf("%d%d", &c, &d);
+		printf("%s\n", (find(c) == find(d) ? "Yes" : "No"));
+		//卡cin,cout
+	}
+	return 0;
+}
